71ed57e84d5d3716b7262ddcae924ac344815b04
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2020-10-21 10:02:33 +0200

    Initial commit

---
+++ src/components/test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Test = () => {
+    return(
+        <div>
+            <h1>React babel webpack setup !!!</h1>
+            <p>This is a ready setup for React.js apps with webpack and babel :) 
+            </p>
+        </div>
+    );
+}
+
+export default Test;
